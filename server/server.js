53be5bf6ae877e352166f45f06b25dacf17e5d68
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2022-08-07 19:35:55 -0300

    feat: websocket server ok

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const httpServer = require("http").createServer();
+const port = 8000;
+// const options = {
+//     /* ... */
+// };
+// const io = require("socket.io")(httpServer, options);
+const io = require("socket.io")(httpServer);
+
+
+io.on("connection", (socket) => {
+    // let address = socket.handshake.address;
+    // let clientIp = socket.request.connection.remoteAddress;
+    // console.log(`IP: ${clientIp}`)
+    // console.log(`Cliente conectado: ${address.address}:${address.port}`)
+    console.log("Cliente conectado: " + socket.id);
+
+    socket.on('webchat', (message) => {
+        console.log('[SOCKET] Webchat:', message)
+        io.emit('webchat', message)
+    })
+
+    socket.on('disconnect', () => {
+        console.log('[SOCKET] Disconnect')
+    })
+})
+
+httpServer.listen(port);
