51fcb51458ea81035deca4463995fe4246e36772
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2022-08-08 23:07:19 -0300

    feat: update middleware

--- middleware/balancer.js
+++ middleware/balancer.js
@@ -3,22 +3,35 @@ const http = require('http');
 const { host, balancer: port, getServerPort } = require('./config');
 
 const balancer = http.createServer();
+const io = require("socket.io")(balancer);
+
 balancer.listen(port, host, () => {
-  console.log(`Load balancer rodando na porta ${port}`);
+    io.on("connection", (socket) => {
+        console.log("Cliente conectado: " + socket.id);
+
+        socket.on("webchat", (message) => {
+            console.log("[SOCKET] Webchat:", message);
+            io.emit("webchat", message);
+        });
+
+        socket.on("disconnect", () => {
+            console.log("[SOCKET] Disconnect");
+        });
+    });
 });
 
 
 balancer.on('request', (req, res) => {
-  const port = getServerPort();
-  const request = {
-    host,
-    port,
-    path: req.url,
-    method: req.method,
-    headers: req.headers
-  };
+    const port = getServerPort();
+    const request = {
+        host,
+        port,
+        path: req.url,
+        method: req.method,
+        headers: req.headers
+    };
 
-  const connector = http.request(request, (resp) => resp.pipe(res));
+    const connector = http.request(request, (resp) => resp.pipe(res));
 
-  req.pipe(connector);
+    req.pipe(connector);
 });
--- middleware/config.js
+++ middleware/config.js
@@ -1,30 +1,22 @@
 const config = {
-    host: 'localhost',
+    host: "localhost",
     balancer: 8000,
-    servers: [
-        8001,
-        8002,
-        8003,
-    ],
+    servers: [8001, 8002],
 
     getServerPort() {
-        console.log('oi')
         // verificamos se é possível incrementar + 1
         // no índice das portas em `server`
-        console.log(config.currentServerPort)
-        console.log(config.servers.length - 1)
-        const canIncreaseServerPort = config.currentServerPort < config.servers.length - 1;
-        console.log(canIncreaseServerPort)
+        const canIncreaseServerPort =
+            config.currentServerPort < config.servers.length - 1;
         // se possível, incrementamos + 1, caso contrário, voltamos à porta inicial
         const nextPort = canIncreaseServerPort ? config.currentServerPort + 1 : 0;
-        console.log(nextPort)
         // armazenamos essa configuração na variável config.currentServerPort
         config.currentServerPort = nextPort;
 
         // retornamos a porta correta através de config.servers
-        console.log(config.servers[nextPort])
+        console.log(config.servers[nextPort]);
         return config.servers[nextPort];
-    }
+    },
 };
 
 module.exports = config;
