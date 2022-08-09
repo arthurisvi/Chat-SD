a77654fea601fe4ceb0fa3f6cfd86590da46119b
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2022-08-09 02:08:59 -0300

    feat: new websocket server

---
+++ secondServer/server.js
@@ -0,0 +1,22 @@
+const http = require("http");
+const port = 8002;
+const server = http.createServer();
+const io = require("socket.io")(server);
+
+server.listen(port, () => {
+    console.log("Servidor subiu na porta " + port);
+    io.on("connection", (socket) => {
+        console.log("Cliente conectado: " + socket.id + "na porta " + port);
+
+        // socket.on("webchat", (message) => {
+        //     console.log("[SOCKET] Webchat:", message);
+        //     io.emit("webchat", message);
+        // });
+
+        // socket.on("disconnect", () => {
+        //     console.log("[SOCKET] Disconnect");
+        // });
+    });
+});
+
+module.exports = io;
